6cc1e81877475fca9fb23cb2a449a1293cddaef1
Anibal <joan.maldonadoa@gmail.com>
2022-07-22 21:27:44 -0400

    Componen test

---
+++ test/components/GifItem.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components/GifItem";
+
+describe("Pruebas en <GifItem />", () => {
+  const title = "Saitama";
+  const url = "https://one-punch.com/jj.jpg";
+
+  test("Debe de hacer match con el snapshot", () => {
+    const { container } = render(<GifItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Debe de mostrar la imagen con el url y el alt indicado", () => {
+    render(<GifItem title={title} url={url} />);
+
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(alt);
+  });
+
+  test("Debe de mostrar el titulo del componente", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
